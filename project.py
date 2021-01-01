2e063c720e73b3d1a4d0418cb9c598935ac72f0e
Rased002 <72910525+Rased002@users.noreply.github.com>
2021-01-02 01:26:57 +0300

    Update project.py
    An Example:
    
    url: https://www.youtube.com/watch?v=L5CV53wCWO0&ab_channel=WindFang%3AD
    
    Video qualities:[1080, 720, 480, 360, 240, 144] 
    
    Audio qualities: [160, 128, 70, 50]
    
    Prefered Video quality: 1080
    
    Prefered Audio quality: 160

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 from pytube import YouTube
 import os.path
+import re
 
-def combine_audio(vidname, audname, outname, fps=25):
+def combine_audio(vidname, audname, outname, fps=30):
     import moviepy.editor as mpe
     
     my_clip = mpe.VideoFileClip(vidname)
@@ -9,34 +10,65 @@ def combine_audio(vidname, audname, outname, fps=25):
     final_clip = my_clip.set_audio(audio_background)
     final_clip.write_videofile(outname, temp_audiofile='temp-audio.m4a', remove_temp=True, fps=fps, codec='libx264', audio_codec='aac')
 
-print('Audio quality is always the best.')
-quality = input('Video quality: ')
-url = input('url: ')
+
+url = input('url: \n')
 
 yt = YouTube(url)
 stream_list = yt.streams
 
+video_list = set()
+audio_list = set()
+
+for i in stream_list:
+    
+    try:
+        if i.includes_audio_track:
+            audio_list.add(int(re.findall('[0-9]+(?=kbps)',str(i))[0]))
+        if i.includes_video_track:    
+            video_list.add(int(re.findall('[[0-9]+(?=p)',str(i))[0]))    
+    except: 
+        continue
+
+print(f'Video qualities:{sorted(video_list,reverse=True)}', f'\n\nAudio qualities: {sorted(audio_list,reverse=True)}')
+video_quality = input('Prefered Video quality: ') + 'p'
+audio_quality = input('Prefered Audio quality: ') + 'kbps'
+output_video_format = input('Prefered output format: ')
+##%%
 
 video_bool = False
 audio_bool = False
 
-for i in range(len(stream_list)):
+for i in stream_list:
 
-    if quality in str(stream_list[i]) and video_bool == False and stream_list[i].mime_type == 'video/mp4':
-        stream_list[i].download(filename = 'video')
+    if video_quality in str(i) and video_bool == False:
+        i.download(filename = 'video')
+        video_format = i.subtype
         video_bool == True
     
-    if 'audio' in str(stream_list[i]) and audio_bool == False:
-        stream_list[i].download(filename ='audio')
+    if audio_quality in str(i) and audio_bool == False:
+        i.download(filename ='audio')
+        audio_format = i.subtype
         audio_bool = True
+        
+    if video_bool and audio_bool:
+        break
+
+audio_file = 'audio.' + audio_format
+video_file = 'video.' + video_format
 
 while(True):
     
-        if os.path.isfile('audio.mp4') and os.path.isfile('video.mp4'):
+        if os.path.isfile(audio_file) and os.path.isfile(video_file):
             break
 
-combine_audio('video.mp4', 'audio.mp4', 'out.mp4', fps = 30)
+combine_audio(video_file, audio_file, 'out.'+output_video_format)
 
-os.remove('audio.mp4')
-os.remove('video.mp4')
+try:
+    os.remove(video_file)
+    os.remove(audio_file)
+    os.remove('video.webm')
+except:
+    pass
+
+
 
